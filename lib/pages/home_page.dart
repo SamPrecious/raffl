e8887e99331ff2b9da1df61e1b33707da21cdbcf
SamPrecious <69593235+SamPrecious@users.noreply.github.com>
2024-02-14 19:00:55 +0000

    Added initial implementation of Navigation and Search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,8 +20,39 @@ class _HomePageState extends State<HomePage> {
     final user = FirebaseAuth.instance.currentUser!; //Gets user information
 
     return Scaffold(
+
       body: Column(
         children: [
+          SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: SearchAnchor(
+                builder: (BuildContext context, SearchController controller) {
+                  return SearchBar(
+                    controller: controller,
+                    onTap: () {
+                      controller.openView();
+                    },
+                    onChanged: (_){
+                      controller.openView();
+                    },
+                    leading: const Icon(Icons.search),
+                  );
+                  }, suggestionsBuilder: (BuildContext context, SearchController controller) {
+                  return List<ListTile>.generate(5, (int index) {
+                    final String item = 'item $index';
+                    return ListTile(
+                      title: Text(item),
+                      onTap: () {
+                        setState(() {
+                          controller.closeView(item);
+                        });
+                      }
+                    );
+                  });
+                  //Handles the suggestions from the search bar
+                }),
+          ),
           Padding(
             padding: EdgeInsets.all(32),
             child: Center(
@@ -43,20 +74,22 @@ class _HomePageState extends State<HomePage> {
               ),
             )
           ),
-          BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.favorite),
-                  label: 'Watching',
-                ),
 
-              ]),
         ],
-      )
+      ),
+      //TODO Abstract the navbar to a seperate widget so it can be called upon by all classes that use it
+
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Watching',
+            ),
+          ]),
 
     );
   }
