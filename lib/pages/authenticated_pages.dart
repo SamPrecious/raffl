b024e44e1dab6db126771cacbc541ac761c7a951
SamPrecious <69593235+SamPrecious@users.noreply.github.com>
2024-02-29 12:09:34 +0000

    Stopped navbar navigating to the same page if requested
    Navbar now checks if it is trying to navigate to the same page, and stops the program if this attempt happens

--- lib/pages/authenticated_pages.dart
+++ lib/pages/authenticated_pages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:raffl/routes/app_router.gr.dart';
 import 'package:raffl/styles/colors.dart';
 
 /*
@@ -27,10 +28,34 @@ class _AuthenticatedPagesState extends State<AuthenticatedPages> {
             Expanded(child: const AutoRouter()),
             //TODO Change icons to custom icons
             BottomNavigationBar(
+
                 type: BottomNavigationBarType.fixed,
                 backgroundColor: secondaryColor,
                 selectedItemColor: tertiaryColor,
                 unselectedItemColor: primaryColor,
+                onTap: (index) {
+                  final currentRouteName = AutoRouter.of(context).topRoute.name;
+                  switch(index) {
+                    case 0:
+                      if(currentRouteName != HomeRoute.name){
+                        AutoRouter.of(context).push(HomeRoute());
+                      }
+                      break;
+                    case 1:
+                      print("watch");
+                      break;
+                    case 2:
+                      print("wins");
+                      break;
+                    case 3:
+                      //AutoRouter.of(context).push(HomeRoute()); TODO inboxroute
+                      break;
+                    case 4:
+                      print("Selling");
+                      break;
+
+                  }
+                },
                 items: const <BottomNavigationBarItem>[
                   BottomNavigationBarItem(
                     icon: Icon(Icons.home_rounded),
