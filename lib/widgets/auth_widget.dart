8cbe8a29f2da16120e3266ac1b3867de025c4d0c
SamPrecious <Spreciouswork@gmail.com>
2024-04-15 16:51:39 +0100

    Improved password security rules
    Also stopped password security rules displaying on log in screen

--- lib/widgets/auth_widget.dart
+++ lib/widgets/auth_widget.dart
@@ -20,6 +20,7 @@ class _AuthWidgetState extends State<AuthWidget> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   //final userHandler = Get.put(UserDataController());
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -29,116 +30,137 @@ class _AuthWidgetState extends State<AuthWidget> {
           child: Center(
             child: SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7, //Makes sure safe area only takes up 70% of screen
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset('assets/icons/raffl_logo_slanted.png'),
-                      SizedBox(height: 30),
-                      Text(
-                        widget.login ?
-                        "Log In":"Register",
-                        style: myTextStyles.titleText,
-                      ),
-                      const SizedBox(height: 40),
-                      Align( //Pushes email text to left of container
-                        alignment: Alignment.bottomLeft,
-                          child: Text(
-                              'Email',
-                            style: myTextStyles.defaultText,
-                          )
-                      ),
-                      TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(8),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                            ),            filled: true,
-                            fillColor: Colors.grey.shade300,
-                          ),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (email) =>
-                            email != null && !EmailValidator.validate(email) //Checks email is valid
-                          ? 'Enter a valid email'
-                                : null,
-                      ),
-                      const SizedBox(height: 10),
-                      Align(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset('assets/icons/raffl_logo_slanted.png'),
+                        SizedBox(height: 30),
+                        Text(
+                          widget.login ?
+                          "Log In":"Register",
+                          style: myTextStyles.titleText,
+                        ),
+                        const SizedBox(height: 40),
+                        Align( //Pushes email text to left of container
                           alignment: Alignment.bottomLeft,
-                          child: Text(
-                            'Password',
-                            style: myTextStyles.defaultText,
-                          )
-                      ),
-                      TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(8),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                            ),            filled: true,
-                            fillColor: Colors.grey.shade300,
-                          ),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) => value != null && value.length <= 8
-                          ? 'Enter 8 characters minimum'
-                        : null
-                      ),
-                      widget.login ?
-                      GestureDetector(
-                        onTap: () async{
-                          await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Reset link sent to Email'),
-                            ),
-                          );
-                        },
-                        child: Align(
-                            alignment: Alignment.bottomRight,
                             child: Text(
-                              'Forgot Password?',
-                              style: myTextStyles.fadedText,
+                                'Email',
+                              style: myTextStyles.defaultText,
                             )
                         ),
-                      ):
-                      const SizedBox(height: 20),
-                      ElevatedButton(onPressed: (){
-                        if(widget.login){
-                          AccessAuth.loginUser(context, emailController.text.trim(), passwordController.text.trim());
-                          widget.onResult.call(true);
-                        }
-                        else{
-                          AccessAuth.registerUser(context, emailController.text.trim(), passwordController.text.trim());
-                          widget.onResult.call(true);
-                        }
-                        //print(emailController.text);
-                        },
-                        style: standardButton,
-                        child: widget.login ? //If Login mode, Login is first button
-                        Text('Login') : Text('Register')
-                      ),
-                      const SizedBox(height: 120),
-                      widget.login ?
-                      Text(
-                        'New?',
-                        style: myTextStyles.fadedText,
-                      ):
-                      Text(
-                        'Have an account?',
-                        style: myTextStyles.fadedText,
-                      ),
-                      ElevatedButton(onPressed: (){
-                        print(emailController.text);
-                        print('${widget.login}');
-                        widget.onClickedSwapState(); //Indicates that we clicked to swap state
-                        },
+                        TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(8),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),            filled: true,
+                              fillColor: Colors.grey.shade300,
+                            ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (email) =>
+                              email != null && !EmailValidator.validate(email) //Checks email is valid
+                            ? 'Enter a valid email'
+                                  : null,
+                        ),
+                        const SizedBox(height: 10),
+                        Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              'Password',
+                              style: myTextStyles.defaultText,
+                            )
+                        ),
+                        TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(8),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),            filled: true,
+                              fillColor: Colors.grey.shade300,
+                            ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                              if(!widget.login){
+                                if (value == null || value.length < 8) {
+                                  return 'Enter 8 characters minimum';
+                                  //The rest uses RegEx to verify certain criteria
+                                } else if (!RegExp(r'^(?=.*[A-Z])').hasMatch(value)) {
+                                  return 'Must contain 1 uppercase character';
+                                } else if (!RegExp(r'^(?=.*[a-z])').hasMatch(value)) {
+                                  return 'Must contain 1 lowercase character';
+                                } else if (!RegExp(r'^(?=.*\d)').hasMatch(value)) {
+                                  return 'Must contain 1 number';
+                                } else if (!RegExp(r'^(?=.*?[!@#\$&*~]).{8,}$').hasMatch(value)) {
+                                  return 'Must contain 1 special character';
+                                }
+                              }
+
+                            return null;
+                          }
+                        ),
+                        widget.login ?
+                        GestureDetector(
+                          onTap: () async{
+                            await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Reset link sent to Email'),
+                              ),
+                            );
+                          },
+                          child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: Text(
+                                'Forgot Password?',
+                                style: myTextStyles.fadedText,
+                              )
+                          ),
+                        ):
+                        const SizedBox(height: 20),
+                        ElevatedButton(onPressed: (){
+                          if(widget.login){
+                            AccessAuth.loginUser(context, emailController.text.trim(), passwordController.text.trim());
+                            widget.onResult.call(true);
+                          }
+                          else if(formKey.currentState?.validate() ?? false){
+
+                            AccessAuth.registerUser(context, emailController.text.trim(), passwordController.text.trim());
+                            widget.onResult.call(true);
+
+                          }
+
+                          //print(emailController.text);
+                          },
                           style: standardButton,
-                          child: widget.login ? //If Login mode, Register is second button
-                          Text('Register') : Text('Login')
-                      )
-                    ],
+                          child: widget.login ? //If Login mode, Login is first button
+                          Text('Login') : Text('Register')
+                        ),
+                        const SizedBox(height: 120),
+                        widget.login ?
+                        Text(
+                          'New?',
+                          style: myTextStyles.fadedText,
+                        ):
+                        Text(
+                          'Have an account?',
+                          style: myTextStyles.fadedText,
+                        ),
+                        ElevatedButton(onPressed: (){
+                          print(emailController.text);
+                          print('${widget.login}');
+                          widget.onClickedSwapState(); //Indicates that we clicked to swap state
+                          },
+                            style: standardButton,
+                            child: widget.login ? //If Login mode, Register is second button
+                            Text('Register') : Text('Login')
+                        )
+                      ],
+                    ),
                   ),
               ),
           ),
